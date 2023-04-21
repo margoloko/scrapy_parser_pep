3ea6d7ff7b76c2fb071dafd376b5beb52f96b393
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-04-21 09:07:47 +0300

    Update items.py
    docstring

--- pep_parse/items.py
+++ pep_parse/items.py
@@ -1,13 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class PepParseItem(scrapy.Item):
-
+    """Класс для хранения данных PEP."""
     number = scrapy.Field()
     name = scrapy.Field()
     status = scrapy.Field()
