69ed266e90683a814d1ab6b19c97f1946282917b
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-04-21 09:12:43 +0300

    Update pep.py
    docstring

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -4,6 +4,16 @@ from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
+    """
+    Класс для парсинга PEP (Python Enhancement Proposals) с сайта https://peps.python.org/.
+
+    :param name: имя паука
+    :type name: str
+    :param allowed_domains: список доменов, которые паук может посетить
+    :type allowed_domains: list
+    :param start_urls: список URL-адресов, которые паук должен посетить при запуске
+    :type start_urls: list
+    """
     name = 'pep'
     allowed_domains = ['peps.python.org']
     start_urls = ['https://peps.python.org/']
@@ -13,7 +23,7 @@ class PepSpider(scrapy.Spider):
             yield response.follow(pep, callback=self.parse_pep)
 
     def parse_pep(self, response):
-
+        """Метод для парсинга данных PEP."""
         yield PepParseItem({
             'number': response.css('h1::text').re_first(r'PEP (?P<number>\d+)'
                                                         ),
